b733ae09dbb9441e2d35ae55710fe151111b65bd
Jeff <jgunderson819@gmail.com>
2020-01-20 10:53:18 -0600

    added a little more now have to debug and get giphy working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,18 @@
 // set variables
 
-let animal = []
-let api-key = "usWbgNcdu9x7OUBm2UQI9JQqygo7erYf"
+let animals = [hamster, gerbel, cat]
+let apikey = "usWbgNcdu9x7OUBm2UQI9JQqygo7erYf"
 
 
 
 // Import giphy
-import GiphyUISDK
-import GiphyCoreSDK 
 
-GiphyUISDK.configure(apiKey: "api-key")
 
   // Function for dumping the JSON content for each button into the div
   function displayGIFInfo() {
 
     var animals = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&apikey="+api-key;
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animals + "&apikey="+apikey;
 
     $.ajax({
       url: queryURL,
@@ -26,28 +23,28 @@ GiphyUISDK.configure(apiKey: "api-key")
   }
 // Search endpoint
 
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+animal+"&api_key="+api-key+"&limit=10");
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+animals+"&api_key="+apikey+"&limit=10");
 xhr.done(function(data) { console.log("success got data", data); });
 
- // Function for displaying movie data
+ // Function for displaying animal gifs
  function renderButtons() {
 
-    // Deleting the movie buttons prior to adding new movie buttons
+    // Deleting the animal buttons prior to adding new movie buttons
     // (this is necessary otherwise we will have repeat buttons)
     $("#buttons-view").empty();
 
     // Looping through the array of movies
-    for (var i = 0; i < animal.length; i++) {
+    for (var i = 0; i < animals.length; i++) {
 
-      // Then dynamicaly generating buttons for each movie in the array.
+      // Then dynamicaly generating buttons for each animal in the array.
       // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
       var a = $("<button>");
       // Adding a class
-      a.addClass("animals");
-      // Adding a data-attribute with a value of the movie at index i
-      a.attr("data-name", animal[i]);
-      // Providing the button's text with a value of the movie at index i
-      a.text(animal[i]);
+      a.addClass("animal");
+      // Adding a data-attribute with a value of the animal at index i
+      a.attr("data-name", animals[i]);
+      // Providing the button's text with a value of the animal at index i
+      a.text(animals[i]);
       // Adding the button to the HTML
       $("#buttons-view").append(a);
     }
@@ -62,7 +59,7 @@ xhr.done(function(data) { console.log("success got data", data); });
     // This line will grab the text from the input box
     var animal = $("#animal-input").val().trim();
     // The movie from the textbox is then added to our array
-    movies.push(animal);
+    animals.push(animal);
 
     // calling renderButtons which handles the processing of our movie array
     renderButtons();
